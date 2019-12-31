52d2e86924571b963e707d7dca98ea4aadb15143
Dan Hayes <dan.hayes1996@gmail.com>
2019-12-31 23:12:28 +0000

    using std::string instead of char*

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,15 @@
 #include <ncurses.h>
 
 #include <vector>
-#include <iostream>
+#include <string>
+#include <algorithm> 
 
 #define MAX_STRING_LEN 5
 #define ESCAPE_CHAR '\033'
 #define CTRL(c) ((c) & 037)
 
-std::vector<char*> arr;
+std::vector<std::string> arr;
 int x = 0, y = 0;
-//char* demo;
-
 
 void doMoveCursor();
 void doControlKey(int c);
@@ -19,9 +18,7 @@ void render();
 
 int main()
 {
-    //demo = new char[5];
-
-    arr.push_back(new char[MAX_STRING_LEN]);
+    arr.push_back(std::string());
     initscr();
     noecho();
  
@@ -40,81 +37,99 @@ int main()
 void doInput(int c)
 {
 
-	if(c == ESCAPE_CHAR){
+	if(c == ESCAPE_CHAR)
+	{
 		doMoveCursor();
-	} else {
+	} 
+	else 
+	{
 		switch(c) 
 		{
 		    case 8:
 		    case 127:
-		        if(x == 0) {
-		            if(y != 0){
+		        if(x == 0) 
+				{
+		            if(y != 0)
+					{
 		                x = 5;
 		                y--;
 		            }
-		        }else {
-		            arr[y][--x] = '\0';
+		        } 
+				else 
+				{
+		            arr[y].erase(--x, 1);
 		        }
 		        break;
 		    case 10:
 		    case 13:
 		        y++;
 				x = 0;
-				arr.push_back(new char[MAX_STRING_LEN]);
+				arr.push_back(std::string());
 		        break;
 			/*case CTRL('r'):
 				doControlKey(c);
 				break;
 			*/
 		    default:
-				if(x != MAX_STRING_LEN){
-					arr[y][x] = c;
-					x = x + 1 == MAX_STRING_LEN ? x : x + 1;
+				if(x < MAX_STRING_LEN)
+				{
+					arr[y].insert(x, 1, (char)c);
+					x++;// = x + 1 == MAX_STRING_LEN ? x : x + 1;
 				}
 		}
 	}
 }
 
 //arrow key form '\033' then '[' then ('A' || 'B' || 'C' || 'D')
-void doMoveCursor() {
-	getch(); //skip [ character
+void doMoveCursor() 
+{
+	getch(); //skip '[' character
 	switch(getch()) {
 		case 'A' : //up arrow 
-			if(y != 0) y--;
+			if(y != 0) 
+			{
+				y--;
+				x = std::min(x, (int) arr[y].length());
+			}
 			else x = 0;
 			break;
 		case 'B' : //down arrow 
-			if(y != arr.size() - 1) y++; 
-			else x = MAX_STRING_LEN - 1;
+			if(y < arr.size() - 1)
+			{ 
+				y++;
+				x = std::min(x, (int) arr[y].length());
+			} 
+			else x = arr[y].length();
 			break;
 		case 'C' : //right arrow 
-			if(x != MAX_STRING_LEN - 1) x++;
-			else if(y != arr.size() - 1) {
+			if(x < arr[y].length())
+				x++;
+			else if(y != arr.size() - 1) 
+			{
 				y++;
 				x = 0;
 			}
 			break;
 		case 'D' : //left arrow 
-			if(x != 0) x--;
-			else if(y != 0) {
+			if(x != 0) 
+				x--;
+			else if(y != 0) 
+			{
 				y--;
-				x = MAX_STRING_LEN - 1;
+				x = arr[y].length();
 			}
 			break;
 	}
 }
 
-void doControlKey(int c) {
-	arr[y][x++] = '1';
+void doControlKey(int c) 
+{
 }
 
 void render()
 {
     clear();
-    for(int i = 0; i < arr.size(); i++) {
-        printw("%s\n", arr[i]);
-
-    }
+    for(int i = 0; i < arr.size(); i++)
+        printw("%s\n", arr[i].c_str());
 	move(y, x);
-        // printw("\n\nx: %i", x);
-}
+ }
